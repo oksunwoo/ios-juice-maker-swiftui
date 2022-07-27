953e7a3d28f7c5a2d3f6b70a69a840c8d340ee53
oksunwoo <oksunwo@naver.com>
2022-07-27 22:10:53 +0900

    initial commit

---
+++ Juice-maker-SwiftUI/Juice-maker-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Juice-maker-SwiftUI
+//
+//  Created by Sunwoo on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Juice-maker-SwiftUI/Juice-maker-SwiftUI/Juice_maker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Juice_maker_SwiftUIApp.swift
+//  Juice-maker-SwiftUI
+//
+//  Created by Sunwoo on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Juice_maker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
