275f97b22d7b20624bd14cf72b696a7f69bdba5f
oksunwoo <oksunwo@naver.com>
2022-07-28 22:01:14 +0900

    refactor: IconView와 StockView를 통합

--- Juice-maker-SwiftUI/Juice-maker-SwiftUI/ContentView.swift
+++ Juice-maker-SwiftUI/Juice-maker-SwiftUI/ContentView.swift
@@ -37,78 +37,74 @@ struct NavigationSetting: View {
             Rectangle()
                 .frame(height: 0)
                 .background(.gray.opacity(0.2))
-            HStack(spacing: 30) {
-                FruitIconView(fruit: "🍓")
-                FruitIconView(fruit: "🍌")
-                FruitIconView(fruit: "🍍")
-                FruitIconView(fruit: "🥝")
-                FruitIconView(fruit: "🥭")
+            HStack(spacing: 18) {
+                FruitStockView(fruit: "🍓", stock: "10")
+                FruitStockView(fruit: "🍌", stock: "10")
+                FruitStockView(fruit: "🍍", stock: "10")
+                FruitStockView(fruit: "🥝", stock: "10")
+                FruitStockView(fruit: "🥭", stock: "10")
             }
             .padding()
-            HStack(spacing: 18) {
-                FruitStockView(stock: "10")
-                FruitStockView(stock: "10")
-                FruitStockView(stock: "10")
-                FruitStockView(stock: "10")
-                FruitStockView(stock: "10")
-            }
-            .padding()
-            HStack(spacing: 18) {
+            HStack(spacing: 25) {
                 OrderButton(content: "딸기쥬스 주문")
                 OrderButton(content: "바나나쥬스 주문")
                 OrderButton(content: "파인애플쥬스 주문")
                 OrderButton(content: "키위쥬스 주문")
                 OrderButton(content: "망고쥬스 주문")
             }
+            .padding()
+            HStack {
+                OrderButton(content: "딸바쥬스 주문")
+                OrderButton(content: "망키쥬스 주문")
+            }
+            .padding()
         }
     }
 }
 
-struct FruitIconView: View {
+struct FruitStockView: View {
     let fruit: String
+    let stock: String
     
     var body: some View {
-        GeometryReader { geometry in
-            Text(fruit)
-                .font(font(in: geometry.size))
+        VStack {
+            GeometryReader { geometry in
+                Text(fruit)
+                    .font(font(in: geometry.size))
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                    .fill()
+                    .foregroundColor(.gray.opacity(0.2))
+                Text(stock)
+            }
         }
     }
     
     private func font(in size: CGSize) -> Font {
         Font.system(size: size.width * DrawingConstants.fontScale)
     }
-    
-    private struct DrawingConstants {
-        static let fontScale: CGFloat = 0.7
-    }
-}
-
-struct FruitStockView: View {
-    let stock: String
-    
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill()
-                .foregroundColor(.gray.opacity(0.2))
-            Text(stock)
-        }
-    }
 }
 
 struct OrderButton: View {
     let content: String
     
-    var body: some View{
+    var body: some View {
         Button {
             
         } label: {
             Text(content)
                 .foregroundColor(.white)
+                //.font(.system(size: ))
         }
         .background(.blue.opacity(0.8))
         .border(.black)
     }
+}
+
+private struct DrawingConstants {
+    static let cornerRadius: CGFloat = 20
+    static let fontScale: CGFloat = 0.7
 }
 
 
